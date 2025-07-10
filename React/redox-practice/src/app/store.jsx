7597adb308365229c371dd40e7e1032378d3fee4
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-10 14:01:09 +0100

    new

---
+++ React/redox-practice/src/app/store.jsx
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit"
+import { counterSlice } from "../feautures/counter/counterSlice"
+
+export const store = configureStore({
+  reducer: {
+    counter: counterSlice
+  }
+})
