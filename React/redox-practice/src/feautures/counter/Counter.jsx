13069f2e70f815b9aa2d3a185d276c503baf281c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-10 14:14:28 +0100

    new

--- React/redox-practice/src/feautures/counter/Counter.jsx
+++ React/redox-practice/src/feautures/counter/Counter.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Counter = ()=> {
     return (
-        <div>Counter</div>
+        <div>
+            <h1>count: 0</h1>
+            b
+        </div>
     )
 }
 export default Counter;
