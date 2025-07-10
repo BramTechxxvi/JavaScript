87e52d3136793cdd37db30817d2d3f05b9c317ac
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-10 14:29:26 +0100

    new

--- React/redox-practice/src/app/store.jsx
+++ React/redox-practice/src/app/store.jsx
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit"
-import { counterSlice } from "../feautures/counter/counterSlice"
+import CounterReducer from "../feautures/counter/counterSlice"
 
 export const store = configureStore({
   reducer: {
-    counter: counterSlice
+    counter: CounterReducer
   }
 })
 
--- React/redox-practice/src/feautures/counter/Counter.jsx
+++ React/redox-practice/src/feautures/counter/Counter.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { counterSlice } from './counterSlice';
 
 const Counter = ()=> {
     return (
